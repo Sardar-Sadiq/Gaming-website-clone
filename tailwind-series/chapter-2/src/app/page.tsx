e98803917138f2c0d212397f9cebc4ddfd0460fd
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-12 00:02:33 +0530

    commit

---
+++ tailwind-series/chapter-2/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-center text-2xl h-[100vh] w-auto bg-red-700">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
