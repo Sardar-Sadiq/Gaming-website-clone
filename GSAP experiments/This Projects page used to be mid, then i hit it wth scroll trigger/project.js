33f9bbc54edd78840a29028b97e75b1d1967e92a
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-26 19:05:54 +0530

    completed the project but facing issue

--- GSAP experiments/This Projects page used to be mid, then i hit it wth scroll trigger/project.js
+++ GSAP experiments/This Projects page used to be mid, then i hit it wth scroll trigger/project.js
@@ -2,42 +2,42 @@ export const projects = [
     {
         name: "Batman",
         description: "Goutham city needs me",
-        img: "/assets/1.jpg"
+        img: "assets/1.jpg"
     },
     {
         name: "Spiderman",
         description: "With great power comes great responsibility",
-        img: "/assets/2.jpg"
+        img: "assets/2.jpg"
     },
     {
         name: "Cute-cats",
         description: "Meow Meow",
-        img: "/assets/3.jpg"
+        img: "assets/3.jpg"
     },
     {
         name: "Cute-cats",
         description: "Meow Meow",
-        img: "/assets/4.jpg"
+        img: "assets/4.jpg"
     },
     {
         name: "Fading Memories",
         description: "When I'm back in Chicago, I feel it Another version of me",
-        img: "/assets/5.jpg"
+        img: "assets/5.jpg"
     },
     {
         name: "Creation of adam",
         description: "Who are you ?",
-        img: "/assets/6.jpg"
+        img: "assets/6.jpg"
     },
     {
         name: "Lonely",
         description: "exploring movement",
-        img: "/assets/7.jpg"
+        img: "assets/7.jpg"
     },
     {
         name: "Alrajul Aladhi Faqad Qalbatan",
         description: "The man who lost his heart",
-        img: "/assets/8.jpg"
+        img: "assets/8.jpg"
     },
 
 ]
